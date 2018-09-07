c84a1d2aa80b237ee8df150b65ed1db0415ec6cf
Roy Niepes <Roy@SAMCD-15016s-iMac.local>
2018-09-07 23:51:25 +0800

    fixes

--- iframe.php
+++ iframe.php
@@ -22,24 +22,45 @@ class Check {
 		$html = file_get_html($url);
 		$html->find('head',0)->innertext = '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script><script src="media/js/scripts.js"></script><link rel="stylesheet" href="media/css/styles.css" />' . $html->find('head',0)->innertext;
 
+
+		ob_start();
+    var_dump( get_headers('https://www.icis.com/contact/request-a-free-demo-of-the-lng-market-intelligence-soasdasdlution/?commodity=lng&channel=energy&cmpid=EMP|ENER|CHHER-2018-0108-GLOBAL-europeangashubreportjulylngmis&sfid=7012X000001qF2E&') );
+
 		foreach( $html->find('a') as $i => &$a ) {
-			$a->setAttribute('class', 'normallink');
+			
 			$attr = get_headers( $a->href );
 
-			if( $attr[0] == "HTTP/1.1 302 Found" ) {
+
+			switch( $attr[0] ) {
+				case "HTTP/1.1 302 Found":
+				$a->setAttribute('class', 'good-link');
 				$a->href = str_replace("Location: ", "", $attr[3]); 
+
+
+
+
+				break;
+
+			  case "HTTP/1.0 404 Not Found":
+				$a->setAttribute('class', 'error-link');
+			  break;
 			}
 
 		}
 
-		echo $html;
+		$_html = ob_get_clean();
+
+
+		$html->find('body',0)->innertext = '<div class="details-container"><div class="details-inner"><div class="details-position"><pre>' . $_html .  '</pre></div></div></div>' . $html->find('body',0)->innertext;
+
+		return $html;
 
 	}
 
 
 
 	public function run() {
-		$this->getHtml();
+		echo $this->getHtml();
 	}
 
 }
